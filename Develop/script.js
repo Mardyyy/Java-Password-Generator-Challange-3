0d553f8632362a148d5b488ca26a22c27f80261d
Mardyyy <martinapopot@gmail.com>
2023-05-04 15:38:29 -0500

    Added for code and ReadMe

--- Develop/script.js
+++ Develop/script.js
@@ -1,118 +1,146 @@
 // Assignment code here
+// Questions:
+// - Prompt for not selecting any of the chacter types. loop back to getPasswordOptions, i get error that getPasswordOptions isn't a function.
+// - Prompt & function if/else for when character amount isnt 8-128.And how to code the range from 8-128. and loop back to start.
+// - why do i need to refresh page to run generate password again. error.
+// - Turn in challenge
 
-// Array of spectial character to be included in password
-var specialCharacters = ['!','@','#','$','%','^','&','*','(',')','-','-','=','+','[','{',']','}','|',';'];
 
-// Array of numeric characters to be included in password
-var numericCharacters = ['0','1','2','3','4','5','6','7','8','9'];
+var specialCharacters = [""];
 
-// Array of lowercase characters to be included in password
-var lowerCasedCharacters = ['qwertyuiopasdfghjklzxcvbnm '.split(
-  '')];
+var numericCharacters = [""];
 
-// Array of uppercase characters to be included in password
-var upperCasedCharacters = ['QWERTYUIOPASDFGHJKLZXCVBNM '.split(
-  '')];
+var lowerCasedCharacters = [""];
+
+var upperCasedCharacters = [""];
+
 
 // Function to prompt user for password options
-function getPasswordOptions() {
+function getPasswordOptions () {
   getNumberAmount ();
   getSpecialCharacters ();
   getNumericCharacters ();
   getLowerCharacters ();
   getUpperCharacters ();
+
+  if (getPasswordOptions = " ") {
+  // prompt("You need to say yes to at least one group.","");
+    noCharacters ();
+  };
+
 };
 
+function noCharacters () {
+  const response = confirm("You must chose at least one character group.");
+
+  if (response) {
+    return getPasswordOptions ();
+    
+  } else {
+    return getPasswordOptions ();
+    
+  };
+};
+
+// function noNumbers () {
+//   const response = confirm("You must select a number from 8-128.");
+
+//   if (response) {
+//   return getPasswordOptions ();
+  
+//   } else {
+//   return getPasswordOptions();
+  
+//   };
+// };
 
 function getNumberAmount() {
-  var doc = prompt("Please enter some text",
-  "GeeksforGeeks");
-if (doc != null) {
-  document.getElementById("g").innerHTML =
-      "Welcome to " + doc;
-};
-};
+  n = Number(prompt("what character amount do you want for your password (8-128)?",
+  ""));
+  if (n != (8 , 128)) {
+    // n = Number(prompt("You must select a number from 8-128!"))
+    const response = confirm("You must select a number from 8-128!");
+
+    if (response) {
+    return getPasswordOptions ();
+    
+    } else {
+    return;
+    
+    };
+  };
+
+  };
+  
 
 function getSpecialCharacters () {
   const response = confirm("Do You want special characters in your password?");
 
   if (response) {
-    // add code if the user pressed the Ok button
-    console.log("Ok was pressed");
+    specialCharacters = ['!@ #$%^&*()_+-=[]{};'];
+    
   } else {
-    // add code if the user pressed the Cancel button
-    console.log("Cancel was pressed");
+    specialCharacters = [""];
+
   };
 }
 
 function getNumericCharacters () {
-  const response = confirm("Do You want special characters in your password?");
+  const response = confirm("Do You want numbers in your password?");
 
   if (response) {
     // add code if the user pressed the Ok button
-    console.log("Ok was pressed");
+    numericCharacters = ['1324567890'];
   } else {
     // add code if the user pressed the Cancel button
-    console.log("Cancel was pressed");
+    numericCharacters = [""];
   };
 }
 
 function getLowerCharacters () {
-  const response = confirm("Do You want special characters in your password?");
+  const response = confirm("Do You want lower case characters in your password?");
 
   if (response) {
     // add code if the user pressed the Ok button
-    console.log("Ok was pressed");
+    lowerCasedCharacters = ['qwertyuiopasdfghjklzxcvbnm'];
   } else {
     // add code if the user pressed the Cancel button
-    console.log("Cancel was pressed");
+    lowerCasedCharacters = [""];
+ 
   };
 }
 
 function getUpperCharacters () {
-  const response = confirm("Do You want special characters in your password?");
+  const response = confirm("Do You want upper case characters in your password?");
 
   if (response) {
-    // add code if the user pressed the Ok button
-    console.log("Ok was pressed");
+    upperCasedCharacters = ['QWERTYUIOPASDFGHJKLZXCVBNM'];
   } else {
-    // add code if the user pressed the Cancel button
-    console.log("Cancel was pressed");
+    upperCasedCharacters = [""];
   };
 }
+
 // Funciton for getting a random element from an array
-function getRandom(arr) {};
+// function getRandom(arr) {};
+
 
 // Function to generate pasword withh user input
 function generatePassword() {
-  // Basic Generator
   var pass = '';
-  var str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' +
-  'abcdefghijklmnopqrstuvwxyz0123456789@#$';
-    
-  for (let i = 1; i <= 8; i++) {
-      var char = Math.floor(Math.random()
-                  * str.length + 1);
-        
-      pass += str.charAt(char)
+  var str = specialCharacters + numericCharacters
+   + lowerCasedCharacters + upperCasedCharacters;
+ 
+
+  for (let i = 1; i <= n; i++) {
+      var char = Math.floor(Math.random() * str.length + 1);
+      
+      pass += str.charAt(char);
+      
+      
   }
-    
+ 
   return pass;
   
-  // My Generator:
-  // var pass = '';
-  // var str = specialCharacters + numericCharacters 
-  // + lowerCasedCharacters + upperCasedCharacters;
-    
-  // for (let i = 1; i <= getNumberAmount; i++) {
-  //     var char = Math.floor(Math.random()
-  //                 * str.length + 1);
-        
-  //     pass += str.charAt(char)
-  // }
-    
-  // return pass;
-
 };
 
 // Get references to the #generate element
@@ -120,11 +148,12 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  // getPasswordOptions();
+  getPasswordOptions();
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+
 
 }
 
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Java-Password-Generator-Challange-3
-Create an application that an employee can use to generate a random password
+
+## Description
+This challendge was to create an application that an employee can use to generate a random password. Through this I learning how to code through java scipt to meet the acceptance criteria. This involved researching and learning how to create a series of different prompts, that each had their own functions, arrays, variables, and loops. 
+
+## Usage
+
+<a href="https://mardyyy.github.io/Martins-Java-Password-Generator-Challange-3/"> alt="Image of Java-Password-Generator-Challange-3"</a>
+
+## Credits
+
+https://github.com/coding-boot-camp/friendly-parakeet
+  -Provided starter code
+
+## License
+
+Refer to the LICENSE in the repo.
